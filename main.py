2d351f3306cb78f90c5b336cd6249bc7baa30d5c
n8henrie@gmail.com <n8henrie@gmail.com>
2013-05-03 18:20:37 -0600

    Started to add basic error handling.

--- main.py
+++ main.py
@@ -1,50 +1,126 @@
-import os
-from google.appengine.ext.webapp import template
-import cgi
-import datetime
-import urllib
-import wsgiref.handlers
-from google.appengine.ext import webapp
+from __future__ import with_statement
+
+from google.appengine.api import files
+from google.appengine.ext.webapp import template, blobstore_handlers
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import ndb, blobstore, ereporter
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
+import wsgiref.handlers
+import webapp2
 
-            template_values = {
+import os
+import datetime
+import logging
+# import cgi
+# import urllib
+
+ereporter.register_logger()
+
+class MainPage(webapp2.RequestHandler):
+    def get(self, success_status=None, blob_key=None):
+
+        urls = {
+            'icsConverterWebapp Instructional Post':
+            'http://n8henrie.com',
+            'icsConverterWebapp at GitHub':
+            'https://github.com/n8henrie/icsConverterWebapp',
             }
 
-            path = os.path.join(os.path.dirname(__file__), 'index.html')
-            self.response.out.write(template.render(path, template_values))
+        if success_status == None:
+            message = '<p>Welcome to my first attempt at a webapp! <br />If you need instructions, please see the blog post <a target="_blank" href="http://nhenrie.com/">here</a>.</p>'
 
-class UploadHandler(webapp.RequestHandler):
+#            urls.update({ 'another url': 'http://fake.com' })
+
+        if success_status == 'success':
+            message = '''<p>Amazingly... it looks like it may have worked. Click <a href='/download/{0}'>here</a>, then check your downloads folder for a file named "calendar.ics". <b>Before importing to your primary calendar, I highly recommend you either inspect it in a text editor and/or import it into a junk calendar.</b></p><p>While I can\'t think of a way this could lead to data loss, I\'m sure that it\'s possible, and it could most certainly lead to a buch of junk calendar events that would be a pain to (manually) clean out. So create a new calendar and import it there, just to be safe. If it works, delete that junk calendar and import to your real one.</p><p>Hope you\'ve found it helpful!</p>'''.format(blob_key)
+
+#            urls.update({ 'another url': 'http://fake.com' })
+
+        if success_status == 'failure':
+
+            error_dict = {
+            'error1': 'either the file wasn\'t a proper .csv file, or the headers were not *exactly* right.',
+            'error2': 'the headers looked okay, but something went wrong in the actual file conversion. Makes sure it was a .csv file and not some other type of spreasheet format.',
+            'error3': 'there was some kind of problem finalizing the file.',
+            }
+
+            message = '''<p>Well, something didn't work right.</p><p>If I had to guess, I would say {0}</p><p>I <em>highly</em> recommend you read the instructional post at least once.</p>'''.format(error_dict[blob_key])
+
+#            urls.update({ 'another url': 'http://fake.com' })
+
+        template_values = {
+        'message': message,
+        'urls': urls,
+        }
+
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+class GeneratedFiles(ndb.Model):
+    FileName = ndb.StringProperty()
+    BlobKey = ndb.BlobKeyProperty()
+
+class UploadHandler(webapp2.RequestHandler):
     def post(self):
 
         from StringIO import StringIO
         import csv
         import icsConverterWebapp
 
-        csvFile = list(csv.DictReader(StringIO(self.request.get('csvUpload', default_value='')), skipinitialspace = True))
+        csvFile = list(csv.DictReader(StringIO(
+        self.request.get('csvUpload',
+        default_value='')), skipinitialspace = True))
 
         icsFile = icsConverterWebapp.convert(csvFile)
 
-        icsFileJS = icsFile.replace('\r\n','\\n\\\r')
+        if icsFile[0:5] == 'error':
+            self.redirect('failure/%s' % icsFile)
 
-        if icsFile:
-            self.response.headers['Content-Type'] = "application/ics"
-            self.response.headers['Content-Disposition'] = 'attachment; filename=calendar.ics'
-            self.response.write(icsFile)
+        elif icsFile:
+
+            # Create the file
+            ics_blob = files.blobstore.create(mime_type =
+            'application/octet-stream')
+
+            # Open the file and write to it
+            with files.open(ics_blob, 'a') as f:
+              f.write(icsFile)
+
+            # Finalize the file. Do this before
+            # attempting to read it.
+            files.finalize(ics_blob)
+
+            # Get the file's blob key
+            blob_key = files.blobstore.get_blob_key(ics_blob)
+
+            f = GeneratedFiles(FileName =
+            ics_blob, BlobKey = blob_key)
+            f.put()
+
+            self.redirect('/success/%s' % blob_key)
 
         else:
-            self.redirect("/failure")
+            print 'There was no icsFile!'
 
-application = webapp.WSGIApplication([
-    ('/upload', UploadHandler),
-    ('/', MainPage)
+class DLHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, blob_key):
+
+        if not blobstore.get(blob_key):
+            self.error(404)
+        else:
+            self.send_blob(blob_key,save_as='converted.ics')
+
+application = webapp2.WSGIApplication(routes=[
+    webapp2.Route(r'/upload', handler=UploadHandler, name='Upload'),
+    webapp2.Route(r'/download/<blob_key:[a-zA-Z0-9]+>', handler=DLHandler, name='Download'),
+webapp2.Route(r'/<success_status:(success|failure)>/<blob_key:[a-zA-Z0-9]+>', handler=MainPage, name='Success-Failure'),
+    webapp2.Route(r'/', handler=MainPage, name='MainPage'),
 ], debug=True)
-
 
 def main():
     run_wsgi_app(application)
+
 
 if __name__ == '__main__':
     main()
